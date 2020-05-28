14937372fb442a9f95f9b670c9d404244fd4a0e8
Sithija Shehara <nsithijashehara@gmail.com>
2020-05-28 07:34:11 +0530

    removed

--- Angular/MyAngularApp/js/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Thomas on 5/28/2015.
- */
-var app = angular.module("tutorialApp",["tutorialCtrlModule"]);
--- Angular/MyAngularApp/js/controllers/tutorialCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module("tutorialCtrlModule",[])
-
-.controller("TutorialCtrl",["$scope", function($scope){
-    //TutorialCtrl - name of controller
-    //$scope - name of scope parameter
-    $scope.name = "sithija"  //sample var
-}]);
