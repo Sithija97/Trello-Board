a3a899132ade9ada6bb00f94a78ea6cf6470f2c9
Sithija Shehara <nsithijashehara@gmail.com>
2020-06-10 11:08:52 +0530

    form validation

--- Angular/Second/src/app/app.module.ts
+++ Angular/Second/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { FormsComponent } from './forms/forms.component'
+import { FormsComponent } from './forms/forms.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    FormsComponent
+    FormsComponent,
+
   ],
   imports: [
     BrowserModule,
