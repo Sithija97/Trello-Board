27e36c7670a6262ee3106a49ae6fb5376c572350
Sithija Shehara <nsithijashehara@gmail.com>
2020-06-24 22:55:16 +0530

    contril values

--- Angular/Fourth/src/app/app.component.ts
+++ Angular/Fourth/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fourth';
+  registrationForm = new FormGroup({
+    userName: new FormControl('defaultname'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  });
+
+  loadApiData() {
+    this.registrationForm.setValue({
+      userName: 'Sithija',
+      password: '123',
+      confirmPassword: '123',
+      address: {
+        city: 'Maharagama',
+        state: 'arewwala',
+        postalCode: '32350'
+      }
+    });
+
+  }
 }
