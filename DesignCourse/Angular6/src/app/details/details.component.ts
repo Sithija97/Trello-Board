6095b0e0e35fdd8cd531a51183b26fc26fe4af1e
Sithija Shehara <nsithijashehara@gmail.com>
2020-07-14 00:04:27 +0530

    completed with posts and details

--- DesignCourse/Angular6/src/app/details/details.component.ts
+++ DesignCourse/Angular6/src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service'
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-details',
@@ -11,9 +12,14 @@ export class DetailsComponent implements OnInit {
 
   userDetails: Object;
 
-  constructor() { }
+  constructor(private data: DataService, private route: ActivatedRoute) { 
+    this.route.params.subscribe( params => this.userDetails = params.id );
+  }
 
   ngOnInit() {
+    this.data.getUser(this.userDetails).subscribe(
+      data => this.userDetails = data 
+    );
   }
 
 }
