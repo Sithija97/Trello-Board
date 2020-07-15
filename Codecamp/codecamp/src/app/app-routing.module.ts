59d982d60f6592aa3b90fb958443098052a83119
Sithija Shehara <nsithijashehara@gmail.com>
2020-07-15 22:16:20 +0530

    updated angular-codeevolution

---
+++ Codecamp/codecamp/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NewComponent } from './new/new.component';
+
+const routes: Routes = [
+  {
+    path:'home',
+    component: NewComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
