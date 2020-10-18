8f149721859140b4c5146aa8fbfcef187143fba8
Sithija Shehara <nsithijashehara@gmail.com>
2020-10-18 22:53:34 +0530

    GET & POST Requests working

--- New folder/API/app.js
+++ New folder/API/app.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const mongoose = require("./database/mongoose");
+const List = require("./database/models/list");
+const Task = require("./database/models/task");
+
+app.use(express.json()); /*map json*/
+app.use(cors()); /*identify different ports*/
+
+/* Routes */
+app.get('/list', (req, res) => {
+    List.find({})
+    .then((lists) => {
+      res.send(lists);
+    })
+    .catch((err) => {
+      console.log("error occured : " + err);
+    });
+});
+
+app.post('/list', (req, res) => {
+  new List({
+    title: req.body.title,
+  })
+    .save()
+    .then((lists) => {
+      res.send(lists);
+    })
+    .catch((err) => {
+      console.log("error occured : " + err);
+    });
+});
 
 app.listen(3000, () => {
   console.log("Server connected to port 3000");
--- New folder/API/database/mongoose.js
+++ New folder/API/database/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 mongoose
   .connect("mongodb://localhost:27017/taskmanager",{ useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Dtabase Connected Successfully !"))
+  .then(() => console.log("Database Connected Successfully !"))
   .catch((error) => console.log("error: " + error));
 
 module.exports = mongoose;
