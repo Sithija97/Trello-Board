938be9db19096663eaa58cf15955fa62a7111bb2
Sithija Shehara <nsithijashehara@gmail.com>
2024-08-10 20:24:24 +0530

    added shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./atoms/ui/button";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <>
+      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </>
+  );
 };
 
 export default App;
